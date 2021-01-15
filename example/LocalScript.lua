c344d0605f765939ab08a483107170ca76356361
ProjektKris <rbxkrisxyz@gmail.com>
2021-01-15 18:15:05 +0700

    Code cleanup

--- example/LocalScript.lua
+++ example/LocalScript.lua
@@ -6,20 +6,10 @@ local UserInputService = game:GetService("UserInputService")
 
 local newChat = chatUI.render()
 
-local focused = false
-
 UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
 	if gameProcessedEvent == false then
-		--print(input.KeyCode)
 		if input.KeyCode == Enum.KeyCode.T then
 			newChat:focusTextBox()
-			focused = true
-		--elseif input.KeyCode == Enum.KeyCode.Return then
-		--	newChat:confirm()
 		end
 	end
-end)
-
-newChat:onFocusLost(function()
-	focused = false
 end)
